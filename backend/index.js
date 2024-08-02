83fe3581ae3dfd871f819bdd780ff9aa4bfa26d8
Arif Basha K. <arifkolimi08@gmail.com>
2024-08-02 18:11:08 +0530

    Fixed hosting problem

--- backend/index.js
+++ backend/index.js
@@ -8,17 +8,23 @@ import { signup } from "./controllers/authControllers.js"
 import { authRouter, postRouter, userRouter } from "./routes/index.js"
 import cookieParser from "cookie-parser"
 import { addPost, updatePost } from "./controllers/postControllers.js"
+import { dirname, join } from "path"
+import { fileURLToPath } from "url"
 
 const app = express()
 const PORT = process.env.PORT || 2357
 
 config()
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+console.log(__dirname);
 //Middleware
 app.use(cors())
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cookieParser());
+app.use(express.static(join(__dirname, "/client/dist")))
 
 //============ CLOUD STORAGE ============//
 cloudinary.config({
@@ -43,6 +49,10 @@ app.put("/api/posts/:id", uploadImage.single("image"), updatePost)
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/posts', postRouter);
+
+//For Render CLI
+app.get("*", (req, res) => res.status(200).sendFile(join(__dirname, "/client/dist/index.html")))
+
 
 app.listen(PORT, () => console.log(`Server is connecting on Port : ${PORT}`))
 
